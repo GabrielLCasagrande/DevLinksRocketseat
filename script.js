4d6e65b905abdb1f1c462d63e3f94f6f929b7542
Gabriel <gabriellourencinicasagrande330@gmail.com>
2023-08-05 08:36:05 -0300

    Add animation to button

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
-
   // substituir a img
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
